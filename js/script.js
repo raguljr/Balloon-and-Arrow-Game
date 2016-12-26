39378996ce3ce859258796d682b4ca7ef76a07e4
raguljr <jothiraj.ragul@gmail.com>
2016-12-26 16:05:54 +0530

    improved gameplay

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     var area = $('.arrow');
     var arrow = $('#arrowObj');
     var bow = $('#bow');
-    //var bubble=$('.bubble');
+    var bubble_0 = $('.bubble');
+    var bubble_1 = $('.bubble1');
+    var bubble_2 = $('.bubble2');
     var currentBubbleX = 0;
     var currentBubbleY = 0;
     var currentBubble1X = 0;
@@ -13,27 +15,14 @@ $(document).ready(function() {
     var currentArrowY = 0;
     var bowX = 0;
     var bowY = 0;
-    var x = 0;
-    var y = 0;
+    var xAxis = 0;
+    var yAxis = 0;
     var tries = 0;
     var hit = 0;
-    var bubble;
-    var bubble_0, bubble_1, bubble_2;
-    bubble_0 = $('.bubble');
-    bubble_1 = $('.bubble1');
-    bubble_2 = $('.bubble2');
 
     function createBaloon() {
 
-        // var a = [bubble_0, bubble_1, bubble_2];
-        // var b = Math.floor(Math.random() * 3);
-
-        //bubble=a[b];
-        var leftPos;
         var p = ["400px", "600px", "800px"];
-        leftPos = p[Math.floor(Math.random() * 3)];
-        //var leftPos=Math.floor(Math.random()*550)+100;
-        // var leftPos="600px";
         bubble_0.css({
             "left": p[0],
             "top": "500px"
@@ -65,41 +54,39 @@ $(document).ready(function() {
         currentBubbleY = parseInt(bubble_0.css("top").slice(0, -2));
         bowX = parseInt(bow.css("left").slice(0, -2));
         bowY = parseInt(bow.css("top").slice(0, -2));
-        //console.log(currentArrow,currentBubble);
+
     }
     setInterval(createBaloon, 2000);
 
     area.on("mousemove", function(event) {
-        x = event.pageX - 40;
-        y = event.pageY;
-        bow.css({ "left": x, "top": y });
-        arrow.css({ "left": x - 40, "top": y });
-        //$( ".pos" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+        xAxis = event.pageX - 40;
+        yAxis = event.pageY;
+        bow.css({ "left": xAxis, "top": yAxis });
+        arrow.css({ "left": xAxis - 40, "top": yAxis });
+
 
     });
 
 
     area.on("mousedown", function(event) {
-        x = event.pageX - 40;
-        y = event.pageY;
-        arrow.css({ "left": x, "top": y });
-        //$( ".pos" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+        xAxis = event.pageX - 40;
+        yAxis = event.pageY;
+        arrow.css({ "left": xAxis, "top": yAxis });
+
         area.off("mousemove");
     });
     area.mouseup(function() {
         tries = tries + 1;
         arrow.animate({ "left": "1050px" }, 2000);
-        // currentArrowX = parseInt(arrow.css("left").slice(0, -2));
-        // currentBubbleY = parseInt(bubble_0.css("top").slice(0, -2));
+
         document.getElementById('playarrow').play();
-        //console.log(currentBubble,currentArrow);
+
 
         area.on("mousemove", function(event) {
-            x = event.pageX - 40;
-            y = event.pageY;
-            bow.css({ "left": x, "top": y });
-            //arrow.css({"left":x-40,"top":y});
-            //$( ".pos" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+            xAxis = event.pageX - 40;
+            yAxis = event.pageY;
+            bow.css({ "left": xAxis, "top": yAxis });
+
 
         });
 
@@ -115,27 +102,20 @@ $(document).ready(function() {
         currentArrowX = parseInt(arrow.css("left").slice(0, -2));
         currentArrowY = parseInt(arrow.css("top").slice(0, -2));
 
-        var x1;
-        var y1 = arrow.offset().top;
-        var x2, x12, x22;
-        var y2 = bubble_0.offset().top;
-        var y12 = bubble_1.offset().top;
-        var y22 = bubble_2.offset().top;
 
+        var arrowTop = arrow.offset().top;
+        var bubble_0Top = bubble_0.offset().top;
+        var bubble_1Top = bubble_1.offset().top;
+        var bubble_2Top = bubble_2.offset().top;
         var hei = parseInt(bubble_0.css("height").slice(0, -2));
-        console.log(hei);
-        //x1=parseInt(x1);
-        x1 = currentArrowX;
-        x2 = currentBubbleX;
-        x12 = currentBubble1X;
-        x22 = currentBubble2X;
 
-        if ((parseInt(y1) >= parseInt(y2) && parseInt(y1) <= parseInt(y2) + 100) && (parseInt(x1) >= parseInt(x2) - 50 && parseInt(x1) <= parseInt(x2) + 50)) {
-            //bubble.css("background-image","url(img/boom.png)");
+
+
+
+        if ((parseInt(arrowTop) >= parseInt(bubble_0Top) && parseInt(arrowTop) <= parseInt(bubble_0Top) + 100) && (parseInt(currentArrowX) >= parseInt(currentBubbleX) - 50 && parseInt(currentArrowX) <= parseInt(currentBubbleX) + 50)) {
+
             hit++;
-
             bubble_0.hide();
-
             document.getElementById('play').play();
 
             function disp() {
@@ -145,8 +125,8 @@ $(document).ready(function() {
 
         }
 
-        if ((parseInt(y1) >= parseInt(y12) && parseInt(y1) <= parseInt(y12) + 100) && (parseInt(x1) >= parseInt(x12) - 50 && parseInt(x1) <= parseInt(x12) + 50)) {
-            //bubble.css("background-image","url(img/boom.png)");
+        if ((parseInt(arrowTop) >= parseInt(bubble_1Top) && parseInt(arrowTop) <= parseInt(bubble_1Top) + 100) && (parseInt(currentArrowX) >= parseInt(currentBubble1X) - 50 && parseInt(currentArrowX) <= parseInt(currentBubble1X) + 50)) {
+
             hit++;
             bubble_1.hide();
 
@@ -159,8 +139,8 @@ $(document).ready(function() {
 
         }
 
-        if ((parseInt(y1) >= parseInt(y22) && parseInt(y1) <= parseInt(y22) + 100) && (parseInt(x1) >= parseInt(x22) - 50 && parseInt(x1) <= parseInt(x22) + 50)) {
-            //bubble.css("background-image","url(img/boom.png)");
+        if ((parseInt(arrowTop) >= parseInt(bubble_2Top) && parseInt(arrowTop) <= parseInt(bubble_2Top) + 100) && (parseInt(currentArrowX) >= parseInt(currentBubble2X) - 50 && parseInt(currentArrowX) <= parseInt(currentBubble2X) + 50)) {
+
             hit++;
             bubble_2.hide();
 
@@ -173,9 +153,7 @@ $(document).ready(function() {
 
         }
 
-        // if((parseInt(y2)>=parseInt(y1) && parseInt(y1)<=parseInt(hei+parseInt(y2)))&&(parseInt(x1)>=parseInt(x2)-50 && parseInt(x1)<=parseInt(x2)+50)){
-        // 		bubble.hide();
-        // }
+
         $('.pos').text("Tries:" + tries + " Hits:" + hit);
 
     }
